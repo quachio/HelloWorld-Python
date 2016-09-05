aa9b59328f3842aa78e0225034e41d989bcf0680
quachio <quachio@icloud.com>
2016-09-05 14:01:08 -0700

    add split string

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,14 +1,22 @@
-print('Hello, World!')
-print(1 + 2)
-print(7 * 6)
-print()
-print("The End")
-print("Python's strings are easy to use")
-print('We can even include "quotes" in string')
-print("hello" + " world")
-greeting = "Hello"
-name = "Bruce"
-print(greeting + name)
-# if we want a space, we can add that too
-print(greeting + ' ' + name)
+# greeting = "Hello"
+# name = input("Please enter your name ")
+# print(greeting + ' ' + name)
+
+# splitString = "This string has been\nsplit over\nserval\nlines"
+# print(splitString)
+
+# tabbedString = "1\t2\t3\4\t5\t"
+# print(tabbedString)
+
+print('The pet show owner say "No, no, \'e\'s uh,...he\s resting')
+print("The pet show owner say \"No, no, 'e's uh,...he's resting\"")
+
+anotherSplitString = """This string has been
+split over
+several lines
+"""
+print(anotherSplitString)
+
+
+
 
