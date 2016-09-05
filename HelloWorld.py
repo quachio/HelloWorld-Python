62efea563bf41c677fbaa2689a7f321a75087b77
quachio <quachio@icloud.com>
2016-09-05 13:03:03 -0700

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+print('Hello, World!')
+print(1 + 2)
+print(7 * 6)
+print()
+print("The End")
+print("Python's strings are easy to use")
+print('We can even include "quotes" in string')
+print("hello" + " world")
+greeting = "Hello"
+name = "Bruce"
+print(greeting + name)
+# if we want a space, we can add that too
+print(greeting + ' ' + name)
+
